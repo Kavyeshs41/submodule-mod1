7df0ce3a9e6143c5685d15cbcfc6e1e1379b85d8
kavyesh.shah <kavyesh.shah@openxcellinc.com>
2020-10-06 23:12:13 +0530

    fix: file path

--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var hellomod1 = require('controllers/hellomode1')
+var hellomod1 = require('./controllers/hellomode1')
 
 router.get('/', hellomod1.helloModule);
 
